aa68301007e314fc8bbb9596256a4af23e9db984
Rajat Sharan <errajatsharan@gmail.com>
2025-10-08 20:32:46 +0530

    adding the datepicker

--- tests/playwright-actions/05-DatePicker/Selecting_Single_Date.test.ts
+++ tests/playwright-actions/05-DatePicker/Selecting_Single_Date.test.ts
@@ -12,9 +12,15 @@ test.beforeEach(async({page})=>{
 
 test("Scenario 1: Selecting a Single Date",async({page})=>{
 
-    const datePicker = page.locator("//input[@id='startDatePicker']")
-    await datePicker.clear()
+    const datePicker = page.locator("//input[@id='departureDateInput']")
     await datePicker.waitFor({ state: "visible" });
     await datePicker.click()
+    const targetDate = new Date('2025-10-11T18:30:00.000Z');
+    const dataDay = targetDate.toISOString();
+    await page.click(`[data-day="${dataDay}"]`);
     await page.pause()
 })
+
+
+
+
